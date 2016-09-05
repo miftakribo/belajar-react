821878408e83bd8dad600c1929850704ea916ea5
miftah <dmiftaranger@gmail.com>
2016-09-05 14:32:01 +0700

    tutorial selesai

--- dev/scripts/tutorial.js
+++ dev/scripts/tutorial.js
@@ -16,6 +16,25 @@ var CommentBox = React.createClass({
 			}.bind(this)
 		});
 	},
+	handleCommentSubmit: function(comment){
+		var comments = this.state.data;
+		comment.id = Date.now();
+		var newComments = comments.concat([comment]);
+		this.setState({data: newComments});
+		$.ajax({
+			url: '/api/add-comment',
+			dataType: 'json',
+			type: 'POST',
+			data: comment,
+			success: function(data){
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err){
+				this.setState({data: data});
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
 	getInitialState: function(){
 		return {data: []};
 	},
@@ -24,11 +43,11 @@ var CommentBox = React.createClass({
 		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
 	},
 	render: function() {
-		return ( 
+		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
 				<CommentList data={this.state.data} />
-				<CommentForm />
+				<CommentForm onCommentSubmit={this.handleCommentSubmit}/>
 			</div>
 		);
 	}
@@ -52,11 +71,42 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+	getInitialState: function(){
+		return {author: '', text: ''};
+	},
+	handleAuthorChange: function(e){
+		this.setState({author: e.target.value});
+	},
+	handleTextChange: function(e){
+		this.setState({text: e.target.value});
+	},
+	handleSubmit: function(e){
+		e.preventDefault();
+		var author = this.state.author.trim();
+		var text = this.state.text.trim();
+		if (!text || !author){
+			return;
+		}
+		this.props.onCommentSubmit({author: author, text: text});
+		this.setState({author: '', text: ''});
+	},
 	render: function(){
 		return (
-			<div className="commentForm">
-				Hello, I am a commentForm
-			</div>
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+				<input
+					type="text"
+					placeholder="your name"
+					value={this.state.author}
+					onChange={this.handleAuthorChange}
+				/>
+				<input
+					type="text"
+					placeholder="say something.."
+					value={this.state.text}
+					onChange={this.handleTextChange}
+				/>
+				<input type="submit" value="Post" />
+			</form>
 		);
 	}
 });
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/dev'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({
+	extended: true
+}));
 
 app.get('/home', function(req,res){
 	res.sendFile(__dirname + '/dev/home.html');
@@ -17,12 +22,21 @@ app.get('/comment-page', function(req, res){
 });
 
 // api comment
+
+var data = [
+	{id: 1, author: "Pete Hunt 2", text: "this is one comment"},
+	{id: 2, author: "Jordan Walke 2", text: "this is *another* comment"},
+];
+
 app.get('/api/comments', function(req, res){
-	res.json([
-		{id: 1, author: "Pete Hunt 2", text: "this is one comment"},
-		{id: 2, author: "Jordan Walke 2", text: "this is *another* comment"},
-	]);
+	res.json(data);
 });
+
+app.post('/api/add-comment', function(sReq, sRes){
+	var counter = data.length + 1;
+	var arr = {id: counter, author: sReq.body.author, text: sReq.body.text}
+	data.push(arr);
+})
 
 app.get('*', function(req, res){
 	res.send('Hello World!');
